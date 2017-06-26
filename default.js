d89aebd34ff32904c5af7c35375f60e1a8dfdfbf
KyknaVerniy <yakinikuteisyoku@outlook.jp>
2017-06-26 17:31:30 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,17 +1,16 @@
 for(var i =1; i < 100; i++){
 var li = document.createElement('li');
-var damage = Math.floor(Math.random() * 100);
-if(damage/3=0||damage/5=0){
+if(i/3=0||i/5=0){
 li.textContent = 'FIZZBUZZ' ;
 }
-else if(damage/5=0){
+else if(i/5=0){
 li.textContent = 'BUZZ';
 }
-else if(damage/3=0){
+else if(i/3=0){
 li.textContent = 'FIZZ';
 }
 else{
-li.textContent = '' + damage;
+li.textContent = '' + i;
 }
 document.getElementById('list').appendChild(li);
 }
