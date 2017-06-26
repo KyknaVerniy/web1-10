55cdc2be8b05628569252be13ab54ce0f7d35594
KyknaVerniy <yakinikuteisyoku@outlook.jp>
2017-06-26 17:35:24 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,12 +1,12 @@
 for(var i =1; i < 100; i++){
 var li = document.createElement('li');
-if(i/3=0||i/5=0){
+if(i%15=0){
 li.textContent = 'FIZZBUZZ' ;
 }
-else if(i/5=0){
+else if(i%5=0){
 li.textContent = 'BUZZ';
 }
-else if(i/3=0){
+else if(i%3=0){
 li.textContent = 'FIZZ';
 }
 else{
